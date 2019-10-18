27aa3ac31bd557bbd271e660c41206fb869432cd
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-19 00:47:43 +0100

    update app

--- routes/client/voyages.js
+++ routes/client/voyages.js
@@ -3,38 +3,23 @@ var voyagesDao = require('../../dao/voyages.dao')
 
 router.post('/', (req, res) => {
   let { nomstation, arrive, date } = req.body
-
-  Promise.all([
-    voyagesDao.getVoyages(),
-    voyagesDao.getVoyageByNomStation(nomstation)
-  ])
+  voyagesDao.getVoyages()
     .then(values => {
-
       date = date && date.trim().length > 4 && Date.parse(date) >= Date.parse(new Date())
         ? date
         : (new Date()).toISOString().slice(0, 10);
 
-      if (arrive && arrive.length > 3) {
-        values[1] = values[1].filter(v => v.arrive === arrive && v.date_depart === date)
-      }
+      let voyages = values.filter(v =>
+        v.nom_station === nomstation && v.arrive === arrive && v.date_depart === date
+      )      
 
-      let compDate = (dp, h, d) => (Date.parse(dp) + (1000 * 60 * 60 * (parseInt(h, 10) - 1))) >= Date.parse(d)
-      let voyages = values[1]
-        .filter(v => v.nb_places > 0 && compDate(v.date_depart, v.heure_depart, new Date()))
-
-      let stations = values[0].filter((v, i, self) =>
+      let stations = values.filter((v, i, self) =>
         i === self.findIndex((t) => (t.nom_station === v.nom_station))
       )
-
-      let arrives = values[0].filter((v, i, self) =>
-        i === self.findIndex((t) => (t.arrive === v.arrive))
-      )
-
-      res.render('client/voyages', { voyages, stations, arrives })
+      res.render('client/voyages', { voyages, stations })
     })
-    .catch(error => {
-      res.render('client/voyages')
-    })
+    .catch(e => { console.log(e);
+     res.render('client/voyages') })
 })
 
 module.exports = router
