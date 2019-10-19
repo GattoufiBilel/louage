cb28122b2b67a8bd63486269469822f5078eab6f
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-19 15:53:25 +0100

    update app

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -4,16 +4,12 @@ fetch('/search')
     var selectSIdx = document.getElementById('select-stations-i')
     var selectSIdxTwo = document.getElementById('select-stations-iv')
     if (selectSIdx && selectSIdxTwo) {
-      r.forEach(v => {
-        selectSIdx.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>`
-        selectSIdxTwo.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>`
-      })
+      r.forEach(v => { selectSIdx.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>` })
+      r.reverse().forEach(v => { selectSIdxTwo.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>` })
     }
     var selectS = document.getElementById('select-stations')
     var selectSTwo = document.getElementById('select-stations-v')
-    r.forEach(v => {
-      selectS.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>`
-      selectSTwo.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>`
-    })
+    r.forEach(v => { selectS.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>` })
+    r.reverse().forEach(v => { selectSTwo.innerHTML += `<option value="${v.nom_station}">${v.nom_station}</option>` })
   })
   .catch(e => { })
