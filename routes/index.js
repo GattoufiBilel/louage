ae067693c36115d257969d49999e0b60be0cdc53
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-19 15:44:53 +0100

    update app

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ var express = require('express')
 var router = express.Router()
 var stationDao = require('../dao/stations.dao')
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.get('/search', (req, res) => {
   stationDao.getStations()
-    .then(stations => { res.render('index', { stations }) })
-    .catch(e => { res.render('index') })
+    .then(stations => { res.status(200).json(stations) })
+    .catch(e => { res.status(404).json([]) })
 })
 module.exports = router
