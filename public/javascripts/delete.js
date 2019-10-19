f86d3da567b8b73dae4b18d475c004292e30d20b
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-19 18:39:38 +0100

    update app

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -25,8 +25,8 @@ btnCancel.forEach(b => {
   b.onclick = (e) => {
     e.stopPropagation()
     swal({
-      title: "Voulez-vous vraiment supprimer?",
-      text: "Une fois vous cliquez supprimer, vous ne pourrez plus le récupérer.",
+      title: "Voulez-vous vraiment annuler?",
+      text: "Une fois vous cliquez annuler, votre reservation sera annulée.",
       icon: "warning",
       buttons: ['non', 'oui'],
       dangerMode: true,
