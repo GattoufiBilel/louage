aa3be03e25c0b9bdadfe4f564770518307b9062d
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-22 18:50:27 +0100

    update app

--- public/javascripts/pages/desactiver.js
+++ public/javascripts/pages/desactiver.js
@@ -2,18 +2,29 @@ document.getElementById('form-desac').onsubmit = (e) => {
   e.preventDefault()
   let password = e.target.password.value;
   let msg = document.getElementById('msg-desc')
-  fetch('/utilisateur/profile/desactiver', {
-    method: 'POST',
-    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-    body: JSON.stringify({ password })
+  swal({
+    title: "Voulez-vous vraiment désactiver?",
+    text: "Une fois vous cliquez désactiver, vous ne pourrez plus récupérer votre compte.",
+    icon: "warning",
+    buttons: ['non', 'oui'],
+    dangerMode: true,
   })
-    .then(r => r.json())
-    .then(r => {
-      if(r.msg.length > 15) {
-        msg.innerHTML = `<div class="alert alert-danger" role="alert"><i class="fas fa-info-circle"></i> ${r.msg}</div>`
-      }
-      else window.location.replace("/register")
+    .then((willDelete) => {
+      if (willDelete) {
+        fetch('/utilisateur/profile/desactiver', {
+          method: 'POST',
+          headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+          body: JSON.stringify({ password })
+        })
+          .then(r => r.json())
+          .then(r => {
+            if (r.msg.length > 15) {
+              msg.innerHTML = `<div class="alert alert-danger" role="alert"><i class="fas fa-info-circle"></i> ${r.msg}</div>`
+            }
+            else window.location.replace("/register")
+          })
+          .catch(e => { msg.innerHTML = `<div class="alert alert-dark" role="alert"><i class="fas fa-info-circle"></i> ${r.msg}</div>` })
+      } else { swal("Désactivation annulée!") }
     })
-    .catch(e => { msg.innerHTML = `<div class="alert alert-dark" role="alert"><i class="fas fa-info-circle"></i> ${r.msg}</div>` })
   return false
 }
