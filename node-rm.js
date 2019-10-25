078016ae4bd4f23d5501c3c58b7e9d0b1e2b3546
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-25 16:18:01 +0100

    update app

--- node-rm.js
+++ node-rm.js
@@ -3,7 +3,7 @@ var { rmSpacesComments, readFiles, getFiles, writeOrCreateFile } = require('node
 let cssFiles = [
   './public/stylesheets/style.css',
   './public/stylesheets/datatable.css',
-  './public/stylesheets/animation.css']
+  './public/stylesheets/animation.css'];
 let content = readFiles(cssFiles)
 
 writeOrCreateFile(rmSpacesComments(content), './public/stylesheets/bundle.css')
