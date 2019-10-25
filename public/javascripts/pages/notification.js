e0d7cc92190b69b7f8cebeda0d49d76b93df6232
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-25 19:54:24 +0100

    update app

--- public/javascripts/pages/notification.js
+++ public/javascripts/pages/notification.js
@@ -1,33 +1,31 @@
-let btnNotif = document.querySelectorAll('.list-group-item')
-if (btnNotif) {
-  btnNotif.forEach(b => {
-    b.onclick = () => {
-      let data = JSON.parse(b.getAttribute('data-notif'));
-      document.querySelector('.modal-body').innerHTML = `
+window.addEventListener('load', () => {
+  let btnNotif = document.querySelectorAll('.list-group-item')
+  if (btnNotif) {
+    btnNotif.forEach(b => {
+      b.onclick = () => {
+        let data = JSON.parse(b.getAttribute('data-notif'));
+        document.querySelector('.modal-body').innerHTML = `
         <div class="d-flex w-100 justify-content-between">
           <h5 class="mb-1" id="sujet">${data.sujet}</h5>
           <small><span class="badge badge-primary">${data.timestamp_notification}</span></small>
         </div>
         <p class="text-break mb-1">${data.message}</p>
         <small><i class="fas fa-map-marker-alt"></i> ${data.nom_station}</small>`
-    }
-  })
-}
+      }
+    })
+  }
 
-window.addEventListener('load', () => {
   function checkNotif (str) {
     return /^[a-z0-9\xBF-\xFF\%\-\+\.\$\/\=\(\)\&\!\?\;\:\, ]+$/gim.test(str)
   }
 
-  let formNotif = document.getElementById('form-notif')
+  let formNotif = document.getElementById('form-notif-ajout') || document.getElementById('form-notif')
   if (formNotif) {
-
-    let NOTIF_PATH = window.location.pathname.split('/').reverse()[0]
-    let NOTIF_URL_API = NOTIF_PATH === 'envoyer' ? '/chefstation/notifications/envoyer' : '/chefstation/notifications/modifier'
+    let NOTIF_URL_API = formNotif.id === 'form-notif-ajout' ? '/chefstation/notifications/envoyer' : '/chefstation/notifications/modifier'
 
     formNotif.onsubmit = (e) => {
       e.preventDefault()
-      let idnotif = NOTIF_PATH === 'envoyer' ? '' : e.target.idnotif.value;
+      let idnotif = formNotif.id === 'form-notif-ajout' ? '' : e.target.idnotif.value;
       let sujet = e.target.sujet.value;
       let msg = e.target.msg.value;
       let msgAlert = document.getElementById('msg-notif')
@@ -40,11 +38,14 @@ window.addEventListener('load', () => {
           .then(r => r.json())
           .then(r => {
             msgAlert.innerHTML = `<div class="alert alert-dark" role="alert"><i class="fas fa-info-circle"></i> ${r.msg}</div>`;
-            if(NOTIF_PATH === 'modifier') {
+            if (formNotif.id !== 'form-notif-ajout') {
               document.getElementById('sujet').textContent = sujet
               document.getElementById('msg').textContent = msg
             }
-            formNotif.reset()
+            else {
+              formNotif.reset()
+              setTimeout(() => { location.reload() }, 2000)
+            }
           })
           .catch(e => {
             msgAlert.innerHTML = `<div class="alert alert-danger" role="alert"><i class="fas fa-info-circle"></i> ${e.msg}</div>`
